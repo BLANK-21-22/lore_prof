eb2297c764d2ed563d35ab9b80edb6f081103f97
engeftroll <engeftroll@gmail.com>
2022-02-20 17:42:17 +0300

    Теперь БД живёт где-то там, в сети...
    Теперь приложение разумно и самостоятельно определяет, какая БД необходима, исходя из переменных окружения.

--- app/configs.py
+++ app/configs.py
@@ -1,14 +1,20 @@
 # Здесь находятся все конфигурационные переменные.
+from os import environ
 
 flask_configs = {
     "DEBUG": True,
     "CACHE_TYPE": "SimpleCache",
     "CACHE_DEFAULT_TIMEOUT": 150
 }
+
 database_configs = {
     "url": "sqlite:///lore_prof.db",
-    "recreate_database": True
+    "recreate_database": False
 }
+
+if environ.get("DATABASE_URL"):
+    print("I am connecting to Database!")
+    database_configs["url"] = environ.get("DATABASE_URL")
 
 token_size = 64
 token_symbols = "qwertyuiopasdfghjklzxcvbnm1234567890"
